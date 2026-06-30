0035438250c4edd1dd55f83a0078e281648afce5
Jafet Poco Chire <jpococ@unsa.edu.pe>
2026-06-29 22:25:36 -0500

    feat: add card component

---
+++ frontend/src/components/card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-white rounded-3xl p-5 shadow-lg border border-slate-100">
+    <div class="flex justify-between items-start">
+      <div>
+        <p class="text-slate-400 font-semibold">
+          {{ title }}
+        </p>
+
+        <h3 class="text-5xl font-black mt-3">
+          {{ value }}
+        </h3>
+      </div>
+
+      <div class="w-14 h-14 rounded-2xl flex items-center justify-center"
+        :class="[bgColor, textColor]">
+        <component :is="icon" class="w-7 h-7" />
+      </div>
+    </div>
+
+    <p class="mt-3 font-bold"
+      :class="textColor">
+      {{ complementInfo }}
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+
+defineProps({
+  title: String,
+  value: Number,
+  icon: Object,
+  complementInfo: String,
+  bgColor: String,
+  textColor: String
+
+});
+
+</script>
--- frontend/src/views/main.vue
+++ frontend/src/views/main.vue
@@ -1,11 +1,69 @@
 <template>
   <LeftMenu>
-    hola Mundo
-    <h1 class="font-bold text-3xl">Primary</h1>
-    <div class="bg-primary-50">primary-50</div>
+    <div class="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-6">
+      <div>
+        <p class="text-slate-500 font-semibold">
+          Bienvenido de nuevo
+        </p>
+        <h2 class="text-4xl font-black mt-2">
+          Hola, [User]
+        </h2>
+      </div>
+
+      <div class="flex gap-4">
+        <button
+          class="h-12 px-6 rounded-2xl bg-white border border-slate-200 font-bold flex items-center gap-3 hover:shadow-md transition text-slate-600/70">
+          <Search class="w-5 h-5 text-slate-400" />
+          Buscar
+        </button>
+
+        <button
+          class="h-12 px-5 rounded-2xl bg-primary-500 text-white font-bold shadow-lg flex items-center gap-3 hover:scale-[1.02] transition duration-300">
+          <Plus class="w-5 h-5" />
+          Nueva tarea
+        </button>
+      </div>
+
+    </div>
+
+    <div class="grid md:grid-cols-3 gap-6 mt-5">
+      <Card 
+        title="Tareas completadas"
+        :value="infoValues.completedTasks"
+        :icon="CheckCircle"
+        complement-info="+12% esta semana"
+        bgColor="bg-primary-100"
+        textColor="text-primary-500">
+      </Card>
+      <Card 
+        title="XP Ganada"
+        :value="infoValues.xpEarned"
+        :icon="Zap"
+        complement-info="Nivel 12 desbloqueado"
+        bgColor="bg-warning-100"
+        textColor="text-warning-500">
+      </Card>
+      <Card 
+        title="Ranking Global"
+        :value="infoValues.globalRanking"
+        :icon="Trophy"
+        complement-info="Top 10 esta semana"
+        bgColor="bg-success-100"
+        textColor="text-success-500">
+      </Card>
+    </div>
   </LeftMenu>
 </template>
 <script setup lang="ts">
+import Card from '@/components/card.vue';
 import LeftMenu from '@/components/leftMenu.vue';
+import { CheckCircle, Plus, Search, Trophy, Zap } from 'lucide-vue-next';
+
+// Prueba Valores
+const infoValues = {
+  completedTasks: 120,
+  xpEarned: 3500,
+  globalRanking: 8
+};
 
 </script>
