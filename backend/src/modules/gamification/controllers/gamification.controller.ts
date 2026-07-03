b53753e7dac64d43e342e89812e343da4dd8b996
Berly Dueñas <bduenasm@unsa.edu.pe>
2026-07-02 21:58:31 -0500

    Added controllers for gamification module

--- backend/src/modules/gamification/controllers/gamification.controller.ts
+++ backend/src/modules/gamification/controllers/gamification.controller.ts
@@ -1,46 +1,53 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
+import { GamificationModel } from '../dto'
+import { GamificationService } from '../services'
 
-export const gamificationController = new Elysia({
-    prefix: '/gamification',
-})
-    .get('/leaderboard', () => {
-        // TODO: return global leaderboard
-        return { message: 'TODO: get leaderboard' }
-    })
+export const gamificationController = new Elysia({ prefix: '/gamification' })
     .get(
-        '/achievements',
-        () => {
-            // TODO: return unlocked achievements for the provided user
-            return { message: 'TODO: get achievements' }
+        '/leaderboard',
+        async ({ query }) => {
+            return await GamificationService.getGlobalLeaderboard(query.limit ?? 10)
         },
         {
-            query: t.Object({
-                userId: t.String(),
-            }),
+            query: GamificationModel.leaderboardQuery,
+            response: {
+                200: GamificationModel.leaderboardResponse,
+            },
+        },
+    )
+    .get(
+        '/achievements',
+        async ({ query }) => {
+            return await GamificationService.getUnlockedAchievements(query.userId)
+        },
+        {
+            query: GamificationModel.userIdQuery,
+            response: {
+                200: GamificationModel.achievementsResponse,
+            },
         },
     )
     .get(
         '/inventory',
-        () => {
-            // TODO: return inventory for the provided user
-            return { message: 'TODO: get inventory' }
+        async ({ query }) => {
+            return (await GamificationService.getInventory(query.userId)) as any
         },
         {
-            query: t.Object({
-                userId: t.String(),
-            }),
+            query: GamificationModel.userIdQuery,
+            response: {
+                200: GamificationModel.inventoryResponse,
+            },
         },
     )
     .post(
         '/inventory/use',
-        () => {
-            // TODO: use item for the provided user
-            return { message: 'TODO: use item' }
+        async ({ body }) => {
+            return await GamificationService.useItem(body.userId, body.itemId)
         },
         {
-            body: t.Object({
-                userId: t.String(),
-                itemId: t.String(),
-            }),
+            body: GamificationModel.useItemBody,
+            response: {
+                200: GamificationModel.useItemResponse,
+            },
         },
     )
