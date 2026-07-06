9bb1b723d4caefe0588e51115140d5f89c8fec59
fhuaquistoq <fhuaquistoq@gmail.com>
2026-07-06 11:53:32 -0500

    fix script deploy

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -26,7 +26,7 @@ if [[ -z "$compose_file" ]]; then
 fi
 
 echo "Validated remote deploy directory and compose file: $compose_file"
-docker compose -f "$compose_file" down -V
+docker compose -f "$compose_file" down -v
 docker compose -f "$compose_file" pull
 docker compose -f "$compose_file" up -d
 docker compose -f "$compose_file" ps
